81f89b4b77b04695c8d447c7943b6f35ae4b2666
Ng Guoyou <gyng@users.noreply.github.com>
2019-01-02 14:22:50 +0800

    Add basic test for prometheus metrics

--- src/metrics/prometheus.rs
+++ src/metrics/prometheus.rs
@@ -63,7 +63,7 @@ impl Metrics for PrometheusMetrics {
                 gauge.set(result.status_code.parse::<f64>().unwrap_or(-1.0f64));
             }
 
-            let latency_gauge = gauges.get(&format!("{}_latency", tag));
+            let latency_gauge = gauges.get(&format!("{}_latency_ms", tag));
             if let Some(gauge) = latency_gauge {
                 gauge.set(result.latency_ms as f64);
             }
@@ -87,5 +87,60 @@ impl Metrics for PrometheusMetrics {
         } else {
             Err("failed to encode printable output".to_string())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use librcanary::*;
+
+    fn test_targets() -> CanaryTargetTypes {
+        CanaryTargetTypes {
+            http: vec![CanaryTarget {
+                alert: false,
+                basic_auth: None,
+                host: "127.0.0.1".to_string(),
+                interval_s: 10,
+                name: "foo".to_string(),
+                tag_metric: Some("footag".to_string()),
+                tag: None,
+            }],
+        }
+    }
+
+    fn ok_result() -> CanaryCheck {
+        let target = test_targets().http.get(0).unwrap().clone();
+        CanaryCheck {
+            alert: false,
+            latency_ms: 1234,
+            need_to_alert: false,
+            status_code: "200".to_string(),
+            status_reason: "foobar".to_string(),
+            status: Status::Okay,
+            target,
+            time: "1234".to_string(),
+        }
+    }
+
+    #[test]
+    fn it_creates_updates_and_prints_the_metrics_registry() {
+        let targets = test_targets();
+        let metrics: PrometheusMetrics = Metrics::new(&targets);
+
+        let ok = ok_result();
+        metrics
+            .update("footag", &ok)
+            .expect("failed to update metrics");
+
+        let expected = "# HELP footag_latency_ms latency for footag\n\
+                        # TYPE footag_latency_ms gauge\n\
+                        footag_latency_ms 1234\n\
+                        # HELP footag_status status for footag\n\
+                        # TYPE footag_status gauge\n\
+                        footag_status 200\n\
+                        ";
+
+        assert_eq!(metrics.print().unwrap(), expected);
     }
 }
